685e85ea02de278864e0cff7cb33b8d9a9892846
SF <fey@ambihome.com>
2024-09-26 13:17:04 +0200

    .

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # AutopsyCatalog
 
+add to `deps()`
+```
+{:autopsy_catalog, git: "https://github.com/georgfaust/autopsy_catalog"},
+```
+
 ```
 # returns the catalog
 AutopsyCatalog.get()
--- lib/autopsy_catalog.ex
+++ lib/autopsy_catalog.ex
@@ -1,5 +1,5 @@
 defmodule AutopsyCatalog do
-  @catalog_path "catalog.etf"
+  @catalog_path "#{:code.priv_dir(:autopsy_catalog)}/catalog.etf"
   @external_resource @catalog_path
 
   def get do
